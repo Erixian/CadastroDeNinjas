b069f9f48360f06c6c40e5a85c865a8bb53ecded
cibercianocode@gmail.com <cibercianocode@gmail.com>
2025-03-18 20:29:39 -0300

    Criada a Controller [Aula2]

---
+++ src/main/java/dev/erix/CadastroDeNinjas/Controller.java
@@ -0,0 +1,15 @@
+package dev.erix.CadastroDeNinjas;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    //GET PARA "PUXAR" AS INFORMAÇÕES
+    @GetMapping("/boasVindas")// isso faz: "localhost:8080/boasVindas
+    public String boasVindas(){
+        return "Essa é minha primeira mensagem nessa rota";
+    }
+}
